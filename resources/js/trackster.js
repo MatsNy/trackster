7571d70b7563428e369860718a474dd0a1412971
MatsNy <mats.h.nygren@gmail.com>
2017-11-01 18:30:05 -0400

    Second Commit - Trackster Project

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,8 +1,17 @@
 var Trackster = {};
+const API_KEY = '150be6a0dfea5faf9261f1f26e928db5';
+
+$(document).ready(function() {
+
+  $('#search-button').click(function() {
+    Trackster.searchTracksByTitle($('#search-input').val());
+  });
+});
+
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
-  Append each "row" to the container in the body to display all tracks. 
+  Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
 
@@ -14,4 +23,13 @@ Trackster.renderTracks = function(tracks) {
 */
 Trackster.searchTracksByTitle = function(title) {
 
+};
+
+Trackster.searchTracksByTitle = function(title) {
+  $.ajax({
+    url: 'https://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
+    success: function(response) {
+      Trackster.renderTracks(response.results.trackmatches.track);
+    }
+  });
 };
